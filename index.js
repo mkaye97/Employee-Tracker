2870f8f60029a3b0d55af873965147da0be2c1a2
mkaye <mattkaye97@gmail.com>
2023-02-07 01:24:09 -0500

    Inquirer is returning desired values. Set up commented out functions to be called for each choice.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const inquirer = require('inquirer');
 
+const viewRole = require('viewrole');
+const viewEmployee = require('viewrole');
+const addRole = require('viewrole');
+const updateEmployee = require('viewrole');
+const addDepartment = require('viewrole');
+const addEmployee = require('viewrole');
+const viewDepartment = require('viewrole');
+
 const mainMenu = () => {
     inquirer
         .prompt([
@@ -11,37 +19,59 @@ const mainMenu = () => {
             }
         ])
         .then((data) => {
-            console.log(data.options);
 
-            if (data.options = 'View All Departments') {
+            if (data.options === 'View All Departments') {
+                console.log('Option 1');
                 // Run a function that executes a department Query
-            };
-
-            if (data.options = 'View All Roles') {
+            } else if (data.options === 'View All Roles') {
+                console.log('Option 2');
                 // Run a function that executes a role Query
-            };
-
-            if (data.options = 'View All Employees') {
+            } else if (data.options === 'View All Employees') {
+                console.log('Option 3');
                 // Run a function that executes an employee Query
-            };
-
-            if (data.options = 'Add a Department') {
+            } else if (data.options === 'Add a Department') {
+                console.log('Option 4');
                 // Run a function that executes a department Query
-            };
-
-            if (data.options = 'Add a Role') {
+            } else if (data.options === 'Add a Role') {
+                console.log('Option 5');
                 // Run a function that asks for the department info and inserts into the appropriate table
-            };
-
-            if (data.options = 'Add an Employee') {
+            } else if (data.options === 'Add an Employee') {
+                console.log('Option 6');
                 // Run a function that executes a department Query
-            };
-
-            if (data.options = 'Update an Employee Role') {
+            } else if (data.options === 'Update an Employee Role') {
+                console.log('Option 7');
                 // Run a function that executes a department Query
-            };
+            }
         }
         )
-        }
+};
+
+// function viewDepartment = () => {
+
+// };
+
+// function viewRole = () => {
+    
+// };
+
+// function viewEmployee = () => {
+    
+// };
+
+// function addDepartment = () => {
+    
+// };
+
+// function addRole = () => {
+    
+// };
+
+// function addEmployee = () => {
+    
+// };
+
+// function updateRole = () => {
+    
+// };
 
 mainMenu();
