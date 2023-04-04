8dce54f75f2d4465ac371242ff48fe78d5821be2
mkaye <mattkaye97@gmail.com>
2023-04-03 21:03:00 -0400

    Adjusted schema for database

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS kaye_corporation_db;
+
+CREATE DATABASE kaye_corporation_db;
+
+USE kaye_corporation_db;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id),
+    ON DELETE SET NULL
+);
